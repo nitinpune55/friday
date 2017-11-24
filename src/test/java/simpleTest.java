84949cae5a0fa69e04f5f5f31bdb88b16618cd54
root <root@ip-172-31-21-151.ap-south-1.compute.internal>
2017-11-24 20:34:23 +0000

    message

---
+++ src/test/java/simpleTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class simpleTest {
+	@Test
+	public void testSum() {
+		int result=7;
+		assertEquals(7, result);
+	}
+}
